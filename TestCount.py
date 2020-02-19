50965e71a34779f5d9b175c91a3a8a4eeb64a0d3
Uthergogogo <48381328+Uthergogogo@users.noreply.github.com>
2020-02-18 19:46:33 -0500

    Add files via upload

---
+++ TestCount.py
@@ -0,0 +1,13 @@
+from HW04a import parsing_link
+import unittest
+
+
+class TestCount(unittest.TestCase):
+    """ class to test the number of commit """
+    def test_number(self):
+        """ test whether get the correct amount of the commit """
+        self.assertEqual(parsing_link("Uthergogogo"), ['Repo: GitHubApi567 Number of commits: 4', 'Repo: SSW-567 Number of commits: 2', 'Repo: SSW-810 Number of commits: 1', 'Repo: Uther567 Number of commits: 8', 'Repo: UtherTree Number of commits: 2'])
+
+
+if __name__ == '__main__':
+    unittest.main(exit=False, verbosity=2)
