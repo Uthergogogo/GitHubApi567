f8d253a813c592e4792b81f191e52ea94022df28
Uthergogogo <48381328+Uthergogogo@users.noreply.github.com>
2020-02-18 21:07:07 -0500

    Update TestCount.py

--- TestCount.py
+++ TestCount.py
@@ -6,7 +6,7 @@ class TestCount(unittest.TestCase):
     """ class to test the number of commit """
     def test_number(self):
         """ test whether get the correct amount of the commit """
-        self.assertEqual(parsing_link("Uthergogogo"), ['Repo: GitHubApi567 Number of commits: 4', 'Repo: SSW-567 Number of commits: 2', 'Repo: SSW-810 Number of commits: 1', 'Repo: Uther567 Number of commits: 8', 'Repo: UtherTree Number of commits: 2'])
+        self.assertEqual(parsing_link("Uthergogogo"), ['Repo: GitHubApi567 Number of commits: 6', 'Repo: SSW-567 Number of commits: 2', 'Repo: SSW-810 Number of commits: 1', 'Repo: Uther567 Number of commits: 8', 'Repo: UtherTree Number of commits: 2'])
 
 
 if __name__ == '__main__':
